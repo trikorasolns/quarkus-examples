94882e12133b2ca177ff1117bb4f4129e73d4d3f
SirSkizo <angel.casanova@estudiante.uam.es>
2021-09-27 14:17:35 +0200

    test2

--- keycloak/src/main/java/com/trikorasolutions/example/resource/UserResource.java
+++ keycloak/src/main/java/com/trikorasolutions/example/resource/UserResource.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.trikorasolutions.example.model.User;
-import graphql.com.google.common.collect.Lists;
 import io.quarkus.security.identity.SecurityIdentity;
 import io.smallrye.mutiny.Uni;
 import org.jboss.resteasy.reactive.NoCache;
@@ -15,8 +13,6 @@ import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.json.JsonArray;
-import javax.json.JsonArrayBuilder;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -24,9 +20,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @ApplicationScoped
 @Path("/api/users")
@@ -44,26 +37,8 @@ public class UserResource {
   @GET
   @Path("/userinfo")
   @NoCache
-  public Uni<Response> getUserInfo() throws JsonParseException, JsonMappingException, IOException {
-    ObjectMapper mapper = new ObjectMapper();
-
-    ObjectNode user1 = mapper.createObjectNode();
-    user1.put("name", this.keycloakSecurityContext.getPrincipal().getName() );
-
-    ArrayNode attr = mapper.createArrayNode();
-    this.keycloakSecurityContext.getAttributes().forEach((i,j)->attr.add(i));
-
-    user1.set("attributes",attr);
-
-
-    LOGGER.info("security: {}" , mapper.writerWithDefaultPrettyPrinter().writeValueAsString(user1));
-    LOGGER.info("security attributes: {}" , this.keycloakSecurityContext.getAttributes());
-    return Uni.createFrom().item(Response.ok(
-      mapper.writerWithDefaultPrettyPrinter().writeValueAsString(user1)
-//        "\nUSER:" + keycloakSecurityContext.getPrincipal().getName() +
-//        "\nWITH ROLES " + keycloakSecurityContext.getRoles() +
-//        "\nWITH CREDENTIALS " +keycloakSecurityContext.getCredentials()
-    ).build());
+  public Uni<Response> getUserInfo() {
+    return Uni.createFrom().item(Response.ok(this.keycloakSecurityContext.getPrincipal().getName()).build());
   }
 
   @GET
@@ -71,14 +46,37 @@ public class UserResource {
   @NoCache
   public Uni<Response> getRoles() throws JsonParseException, JsonMappingException, IOException {
     ObjectMapper mapper = new ObjectMapper();
-
-    LOGGER.info("out_roles lstRoles mapper: {}",
-      mapper.writerWithDefaultPrettyPrinter().writeValueAsString(this.keycloakSecurityContext.getRoles()));
-
-
-    return Uni.createFrom().item(Response.ok(
-      mapper.writerWithDefaultPrettyPrinter().writeValueAsString(this.keycloakSecurityContext.getRoles()))
-      .build());
+    return Uni.createFrom().item(
+      Response.ok(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(this.keycloakSecurityContext.getRoles()))
+        .build());
   }
+
+  @GET
+  @Path("/attributes")
+  @NoCache
+  public Uni<Response> getAttributes() throws JsonParseException, JsonMappingException, IOException {
+    ObjectMapper mapper = new ObjectMapper();
+
+    ObjectNode user1 = mapper.createObjectNode();
+    user1.put("name", this.keycloakSecurityContext.getPrincipal().getName());
+
+    ArrayNode attr = mapper.createArrayNode();
+    this.keycloakSecurityContext.getAttributes().forEach((i, j) -> attr.add(i));
+    user1.set("attributes", attr);
+
+    this.keycloakSecurityContext.getAttributes().forEach((k, v) -> LOGGER.info("k: {}, v: {}", k, v));
+    LOGGER.info("configuration-metadata, issuer: {}",
+      ((io.quarkus.oidc.OidcConfigurationMetadata) this.keycloakSecurityContext.getAttribute(
+        "configuration-metadata")).getIssuer());
+
+    LOGGER.info("security: {}", mapper.writerWithDefaultPrettyPrinter().writeValueAsString(user1));
+    LOGGER.info("security attributes: {}", this.keycloakSecurityContext.getAttributes());
+    return Uni.createFrom().item(Response.ok(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(user1)
+//        "\nUSER:" + keycloakSecurityContext.getPrincipal().getName() +
+//        "\nWITH ROLES " + keycloakSecurityContext.getRoles() +
+//        "\nWITH CREDENTIALS " +keycloakSecurityContext.getCredentials()
+    ).build());
+  }
+
 
 }
